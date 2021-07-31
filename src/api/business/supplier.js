77fa16d976cef279889abdee383576a3fc45fd81
LianjieXu <812571880@qq.com>
2021-07-31 22:42:38 +0800

    form

--- src/api/business/supplier.js
+++ src/api/business/supplier.js
@@ -4,7 +4,23 @@ import request from '@/utils/request'
 export function listSupplier(query) {
   return request({
     url: '/supplier/getList',
-    method: 'post',
+    method: 'get',
     params: query
+  })
+}
+
+
+export function supplierCreate(data) {
+  return request({
+    url: '/supplier/create',
+    method: 'post',
+    data
+  })
+}
+export function supplierDelete(data) {
+  return request({
+    url: '/supplier/delete',
+    method: 'post',
+    data
   })
 }
--- src/views/supplier/supplier-edit.vue
+++ src/views/supplier/supplier-edit.vue
@@ -5,9 +5,13 @@
         <section class="supplier-detail">
             <el-form size="mini"
                      label-position="right"
+                     class="supplier-detail-form"
+                     :model="formData"
+                     ref="ruleForm"
+                     :rules="rules"
                      label-width="150px">
-                <el-form-item label="会员ID">
-                    <!--  -->
+                <el-form-item label="商品"
+                              prop="productId">
                     <el-select :placeholder="'请选择渠道'"
                                clearable
                                name="project"
@@ -16,22 +20,69 @@
                                remote
                                v-loadmore="loadMore"
                                :remote-method="remoteMethod"
-                               v-model="formData.channel_info">
+                               v-model="formData.productId">
                         <el-option v-for="item in position"
                                    :key="item.value"
                                    :label="item.label"
-                                   :value="`${item.label}_${item.value}`">
+                                   :value="item.value">
                         </el-option>
                     </el-select>
                 </el-form-item>
+                <el-form-item label="商品价格"
+                              prop="productPrice">
+                    <el-input v-model="formData.productPrice">
+                        <template slot="append">元</template>
+                    </el-input>
+
+                </el-form-item>
+                <el-form-item label="商品库存"
+                              prop="productSku">
+                    <el-input v-model="formData.productSku"></el-input>
+                </el-form-item>
+                <el-form-item label="采购数量"
+                              prop="supplierOne">
+                    <el-input v-model="formData.supplierOne"></el-input>
+                </el-form-item>
+                <el-form-item label="采购单价"
+                              prop="supplierPrice">
+                    <el-input v-model="formData.supplierPrice">
+                        <template slot="append">元</template>
+                    </el-input>
+                </el-form-item>
+                <el-form-item label="采购总价">
+                    <el-input :disabled="true"
+                              v-model="supplierTotalPrice">
+                        <template slot="append">元</template>
+                    </el-input>
+                </el-form-item>
+                <el-form-item label="状态"
+                              prop="status">
+                    <el-select v-model="formData.status"
+                               style="width: 100%">
+                        <el-option v-for="(item,key) in statusData"
+                                   :key="key"
+                                   :label="item"
+                                   :value="key"></el-option>
+                    </el-select>
+                </el-form-item>
+                <el-form-item label="结算申请日"
+                              prop="applyDay">
+                    <el-input v-model="formData.applyDay"></el-input>
+                </el-form-item>
             </el-form>
         </section>
-
+        <div slot="footer"
+             class="dialog-footer">
+            <el-button @click="currentValue = false">取 消</el-button>
+            <el-button type="primary"
+                       @click="handleQuery">确 定</el-button>
+        </div>
     </el-dialog>
 </template>
 <script>
     import twoWay from '@/mixins/two-way/two-way'
     import { listGoods } from '@/api/business/goods'
+    import { supplierCreate } from '@/api/business/supplier'
     export default {
         mixins: [twoWay],
         props: {
@@ -45,19 +96,99 @@
             }
         },
         data() {
+            var priceReg = /^\d+(.\d{1,2})?$/;
+            var numReg = /^\d+$/;
+            var productPriceValidator = (rule, value, callback) => {
+                if (value === '') {
+                    callback(new Error('请输入精确到小数点两位的数字'));
+                } else {
+                    if (!priceReg.test(Number(value))) {
+                        callback(
+                            new Error("请输入精确到小数点两位的数字")
+                        );
+                    } else {
+                        callback();
+                    }
+                }
+            }
+            var productSkuValidator = (rule, value, callback) => {
+                if (value === '') {
+                    callback(new Error('请输入整数'));
+                } else {
+                    if (!numReg.test(Number(value))) {
+                        callback(
+                            new Error("请输入整数")
+                        );
+                    } else {
+                        callback();
+                    }
+                }
+            }
+
             return {
                 formData: {
-                    channel_info: ''
+                    productId: '',
+                    productPrice: '',
+                    productSku: '',
+                    status: '',
+                    supplierOne: '',
+                    supplierPrice: '',
+                    supplierTotalPrice: '',
+                    applyDay: ''
+                },
+                rules: {
+                    productId: [
+                        { required: true, message: '请选择商品', trigger: 'change' }
+                    ],
+                    productPrice: [
+                        { required: true, validator: productPriceValidator, trigger: 'change' }
+                    ],
+                    productSku: [
+                        { required: true, validator: productSkuValidator, trigger: 'change' }
+                    ],
+                    supplierOne: [
+                        { required: true, validator: productSkuValidator, trigger: 'change' }
+                    ],
+                    supplierPrice: [
+                        { required: true, validator: productPriceValidator, trigger: 'change' }
+                    ],
+                    status: [
+                        { required: true, message: '请选择状态', trigger: 'change' }
+                    ],
+                    applyDay: [
+                        { required: true, validator: productSkuValidator, trigger: 'change' }
+                    ],
                 },
                 position: [],
                 listInite: [],
                 getAll: false,
-                currentPage: 1
+                currentPage: 1,
+                statusData: {
+                    1: '待供应商确认',
+                    2: '供应商已确认，采购中',
+                    3: '采购完成，为结款',
+                    4: '已借款'
+                }
             }
         },
         computed: {
             getTitle() {
                 return this.edit === 'newForm' ? '新建' : '编辑'
+            },
+            supplierTotalPrice() {
+                let totalPrice = 0
+                if (this.formData.supplierOne && this.formData.supplierPrice) {
+                    let buyPrice = Number(this.formData.supplierPrice)
+                    let buyNum = Number(this.formData.supplierOne)
+                    if (typeof buyPrice === 'number' && typeof buyNum === 'number') {
+                        totalPrice = ((buyPrice * buyNum) * 100 / 100).toFixed(2)
+                    } else {
+                        totalPrice = 0
+                    }
+                } else {
+                    totalPrice = 0
+                }
+                return totalPrice
             }
         },
         created() {
@@ -66,10 +197,30 @@
             }
         },
         mounted() {
-
             this.loadList({});
         },
         methods: {
+            handleQuery() {
+                this.$refs.ruleForm.validate((valid) => {
+                    if (valid) {
+                        const params = { ...this.formData }
+                        params.supplierTotalPrice = this.supplierTotalPrice
+                        console.log(params);
+                        supplierCreate(params).then(() => {
+                            this.$notify.success({
+                                title: '成功',
+                                message: '创建成功'
+                            })
+                            this.$emit('refechList')
+                        }).catch(response => {
+                            this.$notify.error({
+                                title: '失败',
+                                message: response.data.errmsg
+                            })
+                        })
+                    }
+                });
+            },
             remoteMethod(query) {
                 return;
                 this.query = query;
@@ -124,5 +275,8 @@
         display: flex;
         width: 100%;
         justify-content: center;
+    }
+    .supplier-detail .supplier-detail-form {
+        width: 80%;
     }
 </style>
