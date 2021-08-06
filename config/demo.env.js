99eaf64df6b9a06e903beda4d251d341ef4f6531
LianjieXu <812571880@qq.com>
2021-08-06 17:01:16 +0800

    域名修改

--- config/demo.env.js
+++ config/demo.env.js
@@ -1,5 +1,5 @@
 module.exports = {
   NODE_ENV: '"production"',
   ENV_CONFIG: '"dep"',
-  BASE_API: '"http://arge.linde.xin/a/admin"'
+  BASE_API: '"https://youjiacang.com/a/admin"'
 }
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 module.exports = {
   NODE_ENV: '"production"',
   ENV_CONFIG: '"prod"',
-  BASE_API: '"http://arge.linde.xin/a/admin"'
+  BASE_API: '"https://youjiacang.com/a/admin"'
 }
